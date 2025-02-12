ef49d3d8c33628fca807e81e7e517ab4b8adc6a3
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-02-12 21:27:31 +0700

    first commit

---
+++ src/main/java/vn/fpt/tranduykhanh/bookingservicepetshop/BookingservicepetshopApplication.java
@@ -0,0 +1,13 @@
+package vn.fpt.tranduykhanh.bookingservicepetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingservicepetshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingservicepetshopApplication.class, args);
+	}
+
+}
